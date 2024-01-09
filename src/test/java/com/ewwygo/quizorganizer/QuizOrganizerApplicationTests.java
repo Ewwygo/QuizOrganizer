cdcb35371f00f1b593dd10e1bb8339cdb5133a8a
Oleg Kudin <oleg.kudin@hesfintech.com>
2024-01-09 12:01:29 +0300

    init

---
+++ src/test/java/com/ewwygo/quizorganizer/QuizOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewwygo.quizorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizOrganizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
